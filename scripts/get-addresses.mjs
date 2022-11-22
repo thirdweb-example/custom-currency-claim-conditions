5715bb638de412351415c175ae00de5b7165e7d9
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-11-22 06:38:08 +0530

    make necessary updates

--- scripts/get-addresses.mjs
+++ scripts/get-addresses.mjs
@@ -4,27 +4,32 @@ import path from "path";
 
 (async () => {
   const sdk = new ThirdwebSDK("goerli");
+  // replace the addresses and amount with your own
   const collectionAddress = "0x08d4CC2968cB82153Bb70229fDb40c78fDF825e8";
   const tokenAddress = "0x2D2DeDba9D45DDaCd938B9DA5b3ED6F7aA18b5C4";
   const tokenAmount = 1200;
+
   const contract = await sdk.getContract(collectionAddress);
 
   if (!contract) {
     return console.log("Contract not found");
   }
 
+  // getting all the NFTs of the collection
   const nfts = await contract.erc721.getAll();
 
   if (!nfts) {
     return console.log("No NFTs found");
   }
 
+  // creating a new array of addresses
   const csv = nfts.reduce((acc, nft) => {
     const address = nft.owner;
     const quantity = acc[address] ? acc[address] + 1 : 1;
     return { ...acc, [address]: quantity };
   }, {});
 
+  // filtering the addressees
   const filteredCsv = Object.keys(csv).reduce((acc, key) => {
     if (key !== "0x0000000000000000000000000000000000000000") {
       return {
@@ -35,6 +40,7 @@ import path from "path";
     return acc;
   }, {});
 
+  // writing the addresses to a csv file
   const csvString =
     "address,maxClaimable,price,currencyAddress\r" +
     Object.entries(filteredCsv)
